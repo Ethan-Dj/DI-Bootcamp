0ad61e2703aec074ac85d5080ff5e0e179f6e077
Ethan <ethan.djanogly@gmail.com>
2023-02-12 23:12:15 +0200

    correction

--- Week-5/Day-1/Daily Challenge/script.js
+++ Week-5/Day-1/Daily Challenge/script.js
@@ -4,12 +4,13 @@ document.getElementsByTagName("form")[0].addEventListener("submit", displayName)
 function displayName(e){
     e.preventDefault()
     const formData = new FormData(document.getElementById("form"));
+    console.log(formData)
     const data = Object.fromEntries(formData)
     const correct = JSON.stringify(data)
 
     const location = document.getElementById("display")
     const element = document.createElement("p")
 
-    location.appendChild(element).textContent = data
+    location.appendChild(element).textContent = correct
 }
 
